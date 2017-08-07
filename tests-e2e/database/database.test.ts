cce9f93cd898928735f7b417782bfa79adc65613
Josh Crowther <jshcrowthe@gmail.com>
2017-08-07 16:53:47 -0700

    WIP: adding more tests

--- tests-e2e/database/database.test.ts
+++ tests-e2e/database/database.test.ts
@@ -8,26 +8,28 @@ describe('Storage Tests', function() {
     browser.url('http://localhost:5003');
     expect(browser.getTitle()).to.equal('Firebase Database Quickstart');
 
-    browser.execute(
-      function(username, password) {
-        firebase.auth().signInWithEmailAndPassword(username, password);
-      },
-      process.env.FIREBASE_USERNAME,
-      process.env.FIREBASE_PASSWORD
-    );
+    browser.execute(() => {
+      firebase.auth().signInAnonymously();
+    });
 
     // Pause to allow for anonymous sign in (POTENTIAL RACE CONDITION HERE)
     browser.pause(2000);
   });
   it('Should properly upload a file with anonymous auth', function() {
-    const date = new Date().getTime();
-
-    browser.debug();
-
+    const title = `Post at (${new Date().getTime()})`;
     browser.click('#add');
-    browser.setValue('#new-post-title', `Post at (${date})`);
+    browser.setValue('#new-post-title', title);
     browser.setValue('#new-post-message', `Test post`);
 
     browser.click('button[type=submit]');
+
+    // There is a delay here in view switching this could potentially cause
+    // issues
+    browser.pause(500);
+
+    const text = browser.getText(
+      '#user-posts-list .post .mdl-card__title-text'
+    );
+    expect(text).to.equal(title);
   });
 });
