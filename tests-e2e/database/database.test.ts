d32e80dd0e0b0e6f5c1fb7cb1e4a286d7f454ca8
Josh Crowther <jshcrowthe@gmail.com>
2017-08-08 11:32:04 -0700

    WIP: small refactoring

--- tests-e2e/database/database.test.ts
+++ tests-e2e/database/database.test.ts
@@ -2,34 +2,28 @@ declare var browser;
 declare var firebase;
 import { expect } from 'chai';
 
-describe('Storage Tests', function() {
+function createNewPost(title) {
+  browser.click('#add');
+  browser.setValue('#new-post-title', title);
+  browser.setValue('#new-post-message', `Test post`);
+  browser.click('button[type=submit]');
+  // There is a delay here in view switching this could potentially cause
+  // issues
+  browser.pause(500);
+}
+
+describe('Database Tests', function() {
   this.timeout(Infinity);
   beforeEach(function() {
     browser.url('http://localhost:5003');
     expect(browser.getTitle()).to.equal('Firebase Database Quickstart');
 
     browser.execute(() => {
+      firebase.auth().signOut();
       firebase.auth().signInAnonymously();
     });
 
     // Pause to allow for anonymous sign in (POTENTIAL RACE CONDITION HERE)
     browser.pause(2000);
-  });
-  it('Should properly post a new topic', function() {
-    const title = `Post at (${new Date().getTime()})`;
-    browser.click('#add');
-    browser.setValue('#new-post-title', title);
-    browser.setValue('#new-post-message', `Test post`);
-
-    browser.click('button[type=submit]');
-
-    // There is a delay here in view switching this could potentially cause
-    // issues
-    browser.pause(500);
-
-    const text = browser.getText(
-      '#user-posts-list .post .mdl-card__title-text'
-    );
-    expect(text).to.equal(title);
   });
 });
